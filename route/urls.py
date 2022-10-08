156963fa39686610026ffe71976bd28c6aceeb9e
Nurbek <xamrayevnurbek0@gmail.com>
2022-10-08 13:52:20 +0500

    ad

--- route/urls.py
+++ route/urls.py
@@ -3,6 +3,8 @@ from page.views import *
 from rest_framework import routers, serializers, viewsets
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from home import settings
 
 router = routers.DefaultRouter()
 
@@ -13,6 +15,7 @@ router.register(r'productimags', ProductImageViewSet)
 
 urlpatterns = [
 
-]
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
+                                                                                           document_root=settings.MEDIA_ROOT)
 
 urlpatterns += router.urls
