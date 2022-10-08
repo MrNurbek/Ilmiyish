d8c113e9e30a7c15de9e58ab34e73d2e1ab9db0e
Nurbek <xamrayevnurbek0@gmail.com>
2022-10-08 14:46:26 +0500

    ad

--- home/settings.py
+++ home/settings.py
@@ -121,7 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -133,12 +141,4 @@ LOCATION_FIELD = {
     'provider.google.map.type': 'ROADMAP',
 }
 
-STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
