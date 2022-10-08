48df71ab03bf25cb32661c1f07954fabe0b0a180
Nurbek <xamrayevnurbek0@gmail.com>
2022-10-08 14:52:50 +0500

    ad

--- home/settings.py
+++ home/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
